2e5d5bb65da97997662f8e4dbe89c629b1ee8bea
Leonard Tulipan <leonard.tulipan@outlook.com>
2014-09-02 16:59:10 +0200

    Renamed files. Added Readme

--- bawag.js
+++ bawag.js
@@ -1,81 +1,31 @@
-console.log("Starting Slimer/Phantom!");
+console.log("Starting node js wrapper script!");
 
-// Tested with the following version of eBanking
-var version = "14.03.19.08"
-
-var page = require("webpage").create();
-var system = require('system');
-
-var dn = system.env["BAWAG_DN"];
-var pin = system.env["BAWAG_PIN"];
-
-if(isNaN(parseFloat(dn)))
-  {
-    console.log("No Login BAWAG_DN defined. Check your .env")
-    phantom.exit()
-  }
-
-
-page.onConsoleMessage = function (msg) {
-    console.log(msg);
+var escapeshell = function(cmd) {
+  return '"'+cmd.replace(/(["\s'$`\\])/g,'\\$1')+'"';
 };
 
+//var fs = require('fs')
 
-page.open('https://ebanking.bawagpsk.com/InternetBanking/InternetBanking?d=login&svc=BAWAG&ui=html&lang=de', function() {
-
-  page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-
-    page.evaluate(function(version, dn, pin) {
-
-    /* Get eBanking Version string out of comments */
-    var vnr
-    $("body").contents().filter(function(){
-        return this.nodeType == 8;
-    }).each(function(i, e){
-        vnr = e.nodeValue.split(" ")[4].split("-")[1];
-    });
-    if(vnr != version)
-    {
-      console.log("eBanking Version no supported: " + vnr)
-      console.log("Supported Version: " + version)
-      exit();
-    }
-    else
-    {
-       console.log( $("h3").first().text() );
-
-       $("input[name='dn']").val(dn)
-       $("input[name='pin']").val(pin)
-       $("#form").submit()
-      // submitLogin(document.loginForm)
-     console.log( $("#error_part_text").text() );
-     console.log( $(".login-error").text() );
-
-    }
-  }, version, dn, pin );
-    phantom.exit();
-  });
-});
+// Load .env - will be accessed in slimerjs script bawag.js
+var dotenv = require('dotenv');
+dotenv.load();
 
 
-/* page.open("https://ebanking.bawagpsk.com/InternetBanking/InternetBanking?d=login&svc=BAWAG&ui=html&lang=de", function() {
-  page.includeJs("https://code.jquery.com/jquery-2.1.1.js", function() {
-    page.evaluate(function() {
-       page.viewportSize = { width:1024, height:768 };
-       // in case of:  page.render('screenshot.png')
-         if (status == "success") {
-             var mainTitle = page.evaluate(function () {
-                console.log('message from the web page');
-              //  return document.querySelector("h3").textContent;
-              return $("h3").first().text();
-            });
-             console.log("The title of the page is: "+ mainTitle);
-         }
-         else {
-             console.log("Sorry, could not access page. Are your Intertubes connected?");
-         }
-         page.close();
-         phantom.exit();
-       });
-     }); // includeJS
-   }); // page.open */
+var path = require('path')
+var childProcess = require('child_process')
+var phantomjs = require('slimerjs')
+var binPath = (phantomjs.path)
+
+var childArgs = [
+  ('bawag-slimer.js'),
+]
+
+childProcess.execFile( binPath , childArgs, function(err, stdout, stderr) {
+  if(err) {
+    console.log("There was an error executing the slimerjs script:")
+    console.log(err)
+    console.log(stderr)
+  }
+  console.log(stdout)
+  // handle results
+})
