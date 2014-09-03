8ebf776bba7e04edbc494bd718bd8df412ff4b04
Leonard Tulipan <leonard.tulipan@outlook.com>
2014-09-03 18:02:38 +0200

    First "working" version. still buggy but importable

--- csv2qif.js
+++ csv2qif.js
@@ -1,5 +1,5 @@
-var transactions = {
-cash: [
+var transactions = []
+/*cash: [
   {
     date: '3/7/2014',
     amount: -213.39,
@@ -14,7 +14,42 @@ cash: [
     category: 'Dining Out:Coffee'
   }
 ]
-};
+}; */
 
 var qif = require('qif');
-qif.writeToFile(transactions, './out.qif', function (err, qifData) {});
+
+var fs = require('fs')
+var sys = require('sys')
+
+var csv = require('csv-parser')
+
+/*
+Maybe more synchronous with this snippet in the future?
+
+function parseCsvFile(fileName, callback){
+  var stream = fs.createReadStream(fileName)
+*/
+
+fs.createReadStream('my-secret-sample.csv')
+  .pipe(csv({
+    raw: false,    // do not decode to utf-8 strings
+    headers: ["KtoNr", "memo", "Valuta", "date", "amount", "Balance", "Currency"],
+    separator: ';' // specify optional cell separator
+    }))
+  .on('data', function(data) {
+
+    // replace , with . in amount
+    // Umlaut/ UTF8 Bug / Windows Encoding
+    console.log(data["amount"])
+    transactions.push(data)
+}).on('end', function() {
+  console.log("Read", transactions.length, "Transactions from CSV")
+  qif.writeToFile({cash: transactions}, './out.qif', function (err, qifData) {
+    if(err)
+      {
+        console.log("Err: ", err, "qif ", qifData)
+      }
+
+  });
+
+});
