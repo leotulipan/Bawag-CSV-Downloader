af1f844bb5ca936ac880777421f290779ba341a6
Leo Tulipan <Leo@LeoTulipan.at>
2017-11-18 08:26:35 +0100

    read date_field from a var

--- csv2qif.js
+++ csv2qif.js
@@ -7,6 +7,9 @@ var Papa = require('papaparse')
 
 var qif = require('qif');
 
+// Iso Regex http://regexlib.com/REDetails.aspx?regexp_id=3344&AspxAutoDetectCookieSupport=1
+var iso8601_regex = /^(?:(?=[02468][048]00|[13579][26]00|[0-9][0-9]0[48]|[0-9][0-9][2468][048]|[0-9][0-9][13579][26])\d{4}(?:(-|)(?:(?:00[1-9]|0[1-9][0-9]|[1-2][0-9][0-9]|3[0-5][0-9]|36[0-6])|(?:01|03|05|07|08|10|12)(?:\1(?:0[1-9]|[12][0-9]|3[01]))?|(?:04|06|09|11)(?:\1(?:0[1-9]|[12][0-9]|30))?|02(?:\1(?:0[1-9]|[12][0-9]))?|W(?:0[1-9]|[1-4][0-9]|5[0-3])(?:\1[1-7])?))?)$|^(?:(?![02468][048]00|[13579][26]00|[0-9][0-9]0[48]|[0-9][0-9][2468][048]|[0-9][0-9][13579][26])\d{4}(?:(-|)(?:(?:00[1-9]|0[1-9][0-9]|[1-2][0-9][0-9]|3[0-5][0-9]|36[0-5])|(?:01|03|05|07|08|10|12)(?:\2(?:0[1-9]|[12][0-9]|3[01]))?|(?:04|06|09|11)(?:\2(?:0[1-9]|[12][0-9]|30))?|(?:02)(?:\2(?:0[1-9]|1[0-9]|2[0-8]))?|W(?:0[1-9]|[1-4][0-9]|5[0-3])(?:\2[1-7])?))?)$/
+
 var transactions = []
 /*cash: [
   {
@@ -24,6 +27,16 @@ var transactions = []
   }
 ]
 }; */
+
+var payee_transform = {
+  "HEDGEYE RISK MGMT": "Hedgeye",
+  "HOFER DANKT": "Hofer",
+  "AMAZON.CO.UK": "Amazon",
+  "Amazon UK Marketplace": "Amazon",
+  "EASYJET000    ET44K39": "Easyjet"
+  // Easyjet: /^EASYJET/i
+  // PAYPAL *TANGXIAOLAN => "Paypal"
+}
 
 var payees = {
   Hofer: ["HOFER DANKT", "Alltag: Lebensmittel"],
@@ -83,7 +96,7 @@ var mc_date_field = "Buchungsdatum"
 //   }
 // }
 
-function prepare_bawag(data) {
+function parse_bawag(data) {
   console.log("we got data")
   var payee = "",
     category = ""
@@ -153,9 +166,6 @@ function prepare_bawag(data) {
   }
 }
 
-// Iso Regex http://regexlib.com/REDetails.aspx?regexp_id=3344&AspxAutoDetectCookieSupport=1
-var iso8601_regex = /^(?:(?=[02468][048]00|[13579][26]00|[0-9][0-9]0[48]|[0-9][0-9][2468][048]|[0-9][0-9][13579][26])\d{4}(?:(-|)(?:(?:00[1-9]|0[1-9][0-9]|[1-2][0-9][0-9]|3[0-5][0-9]|36[0-6])|(?:01|03|05|07|08|10|12)(?:\1(?:0[1-9]|[12][0-9]|3[01]))?|(?:04|06|09|11)(?:\1(?:0[1-9]|[12][0-9]|30))?|02(?:\1(?:0[1-9]|[12][0-9]))?|W(?:0[1-9]|[1-4][0-9]|5[0-3])(?:\1[1-7])?))?)$|^(?:(?![02468][048]00|[13579][26]00|[0-9][0-9]0[48]|[0-9][0-9][2468][048]|[0-9][0-9][13579][26])\d{4}(?:(-|)(?:(?:00[1-9]|0[1-9][0-9]|[1-2][0-9][0-9]|3[0-5][0-9]|36[0-5])|(?:01|03|05|07|08|10|12)(?:\2(?:0[1-9]|[12][0-9]|3[01]))?|(?:04|06|09|11)(?:\2(?:0[1-9]|[12][0-9]|30))?|(?:02)(?:\2(?:0[1-9]|1[0-9]|2[0-8]))?|W(?:0[1-9]|[1-4][0-9]|5[0-3])(?:\2[1-7])?))?)$/
-
 cmdline
   .version('0.0.3')
   .option('-i, --input [filename]', 'Input file (.csv is added if needed). Default "input.csv"', "input")
@@ -187,15 +197,15 @@ var json_data = Papa.parse(csv_file, mc_csv_config)
 
 // Reformat Date "02.11.2017"
 json_data.data = json_data.data.map(function (line) {
-  line.Buchungsdatum = line.Buchungsdatum.replace(/(\d{1,2})\.(\d{1,2})\.(\d{4})/, "$3-$2-$1")
-  line.Transaktionsdatum = line.Transaktionsdatum.replace(/(\d{1,2})\.(\d{1,2})\.(\d{4})/, "$3-$2-$1")
-  line.Abrechnungsdatum = line.Abrechnungsdatum.replace(/(\d{1,2})\.(\d{1,2})\.(\d{4})/, "$3-$2-$1")
+  line[mc_date_field] = line[mc_date_field].replace(/(\d{1,2})\.(\d{1,2})\.(\d{4})/, "$3-$2-$1")
+  // line.Transaktionsdatum = line.Transaktionsdatum.replace(/(\d{1,2})\.(\d{1,2})\.(\d{4})/, "$3-$2-$1")
+  // line.Abrechnungsdatum = line.Abrechnungsdatum.replace(/(\d{1,2})\.(\d{1,2})\.(\d{4})/, "$3-$2-$1")
   return line
 }).filter(function (
   currentValue
 ) {
   // console.log(new Date(currentValue.Buchungsdatum + "T00:00:00Z"))
-  if (new Date(currentValue.Buchungsdatum) > new Date(cmdline.date)) {
+  if (new Date(currentValue[mc_date_field]) > new Date(cmdline.date)) {
     return currentValue
   }
 })
